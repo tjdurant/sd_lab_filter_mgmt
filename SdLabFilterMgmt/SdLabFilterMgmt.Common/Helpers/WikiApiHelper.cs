39719f8f0cc26672d15b4d759a3b847e1f57be18
Tommy Durant <thomas.durant@yale.edu>
2017-07-31 21:04:16 -0400

    initial commit

---
+++ SdLabFilterMgmt/SdLabFilterMgmt.Common/Helpers/WikiApiHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WikiClientLibrary.Client;
+
+namespace SdLabFilterMgmt.Common.Helpers
+{
+    public class WikiApiHelper
+    {
+        public WikiClient WikiClient;
+
+        public QueryWiki()
+        {
+            
+        }
+    }
+}
